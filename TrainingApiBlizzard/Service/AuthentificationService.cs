b6734b13c81d5befc235abbae89fea849d830169
Thibaud <thibaud.tailly@hotmail.fr>
2024-01-28 21:16:39 +0100

    start_service_Token

---
+++ TrainingApiBlizzard/Service/AuthentificationService.cs
@@ -0,0 +1,32 @@
+﻿namespace TrainingApiBlizzard.Service
+{
+    public class AuthentificationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthentificationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        
+        public async Task<string?> GetAccessTokenAsync()
+        {
+            var clientId = "29dd723dc3104df695a1e9e87b42cdfa";
+            var clientSecret = "jOAu1r5lmE0Tg1H4MXj5uRBs7ZrkJ7pw";
+
+            var tokenUrl = "https://oauth.battle.net/token";
+            var parameters = new Dictionary<string, string>
+            {
+                {"grand_type","client_credentials"},
+                {"client_id",clientId },
+                {"client_secret", clientSecret }
+            };
+
+            // endroit ou receptionner le token de reponse
+            //var reponse 
+            var stringTokenReponse ="";
+            return stringTokenReponse;
+
+        }
+    }
+}
